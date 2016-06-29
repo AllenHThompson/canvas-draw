25458c97a0b82acd7ed7f610eef62d163d00bda5
Allen H. Thompson <allenhthompson1@gmail.com>
2016-06-29 14:39:19 -0400

    Updated connectivity to send position to server and server broadcast to other clients.

--- public/draw-together.js
+++ public/draw-together.js
@@ -9,44 +9,62 @@ var ctx = canvas.getContext('2d');
 var prevX, prevY;
 var mouseDown = false;
 var color;
+var lineWidth;
 
-// if the mouse is down and you are moving the mouse than draw dots
+var serverSocket = io();
+
+
+
+// recieves a msg from the server
+serverSocket.on('draw', function(twoPoints){
+     Draw(twoPoints.x, twoPoints.y, twoPoints.prevX, twoPoints.prevY, twoPoints.color, twoPoints.lineWidth);
+     console.log(twoPoints);
+});
 
 $('#canvas').mousedown(function(event){
      mouseDown = true;
-     Draw(event.pageX - $(this).offset().left, event.pageY - $(this).offset().top);
 });
 
 $('#canvas').mousemove(function(event){
-     console.log((event.pageX + ', ' + event.pageY));
+     var x = event.pageX - $(this).offset().left;
+     var y = event.pageY - $(this).offset().top;
 
-     // function call to draw dots on the canvas
-     Draw(event.pageX - $(this).offset().left, event.pageY - $(this).offset().top);
-
+     if(mouseDown){
+          // sends information to the server
+          serverSocket.emit('draw', {x: x, y: y, prevX: prevX, prevY: prevY, color: color, lineWidth: lineWidth});
+          // function call to draw dots on the canvas
+          Draw(x, y, prevX, prevY, color, lineWidth);
+     }
+     prevX = x;
+     prevY = y;
 });
+
 // if the mouse is up dont draw dots
 $("#canvas").mouseup(function(){
      mouseDown = false;
-     Draw(event.pageX, event.pageY);
+     // Draw(event.pageX, event.pageY);
 });
 
+// gets the data-color from the color btn that was clicked -> saves it to the color variable
 $('#color button').click(function(){
      // var color = $(this).text
      color = $(this).data('color');
-     console.log(color);
 });
 
-function Draw(x,y){
-     if(mouseDown){
-          ctx.strokeStyle = color;
-          ctx.lineWidth = $('#width').val();
-          ctx.lineJoin = 'round';
-          ctx.moveTo(prevX, prevY);
-          ctx.lineTo(event.pageX, event.pageY);
-          ctx.closePath();
-          ctx.stroke();
-     }
+//gets the data-width from the width button that was clicked -> saves it to the lineWidth variable
+$('#width button').click(function(){
+     // var color = $(this).text
+     lineWidth = $(this).data('width');
+});
 
-     prevX = event.pageX;
-     prevY = event.pageY;
+// function that takes in the starting and ending points, color and width of the line and draws the line
+function Draw(x,y,prevX, prevY, color, lineWidth){
+     ctx.strokeStyle = color;
+     ctx.lineWidth = lineWidth;
+     ctx.lineJoin = 'round';
+     ctx.beginPath();
+     ctx.moveTo(prevX, prevY);
+     ctx.lineTo(x,y);
+     ctx.closePath();
+     ctx.stroke();
 }
--- server.js
+++ server.js
@@ -5,6 +5,18 @@ var io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
+io.on('connection', function(clientSocket) {
+     //     console.log('this is the client socket', clientSocket);
+     console.log('a user connected');
+
+     clientSocket.on('draw', function(twoPoints) {
+          clientSocket.broadcast.emit('draw', twoPoints);
+          console.log('hey');
+     });
+});
+
+
+
 
 http.listen(8000, function() {
      console.log('Listening on 8000');
